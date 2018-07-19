825d06e77062a02c82b0cd5d5d253453849cd804
Shoro2 <Assitop@web.de>
2018-07-19 17:21:14 +0200

    stats: elo distribution & win/pickrates

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,3 +1,216 @@
+//show container
+function showStatsPage() {
+    document.getElementById("stats").style.display = "inherit";
+}
+function showLoad() {
+    document.getElementById("mitte").style.display = "inherit";
+}
+function hideStatsPage() {
+    document.getElementById("stats").style.display = "none";
+    myChart.destroy();
+}
+function hideLoad() {
+    document.getElementById("mitte").style.display = "none";
+}
+function showInputs() {
+    document.getElementById("inputs").style.display = "";
+}
+function hideInputs() {
+    document.getElementById("inputs").style.display = "none";
+}
+
+function createLineGraph(data) {
+    var ctx = document.getElementById("myChart");
+    ctx.height = 500;
+    ctx.width = 1000;
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                label: 'Elo Distribution',
+                data: [],
+                backgroundColor: [
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            },
+            title:
+            {
+                display: true,
+                text: "Elo Distribution per Elo in %"
+            }
+        }
+    });
+    var eloDistribution = data.stats.eloDistribution;
+    eloDistribution.forEach(function (element) {
+        addData(myChart, element.percentile, element.elo);
+    });
+}
+
+function createBarGraph(data) {
+    hideLoad();
+    var ctx = document.getElementById("myChart");
+    console.log(data);
+    ctx.height = 500;
+    ctx.width = 1000;
+    myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [],
+            datasets: [{
+                label: null,
+                data: [],
+                backgroundColor: [
+                    'rgba(255, 255, 0, 0.8)',
+                    'rgba(255, 255, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(0, 255, 0, 0.8)',
+                    'rgba(0, 255, 0, 0.8)',
+                    'rgba(0, 0, 0, 0.8)',
+                    'rgba(0, 0, 0, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)'
+                ],
+                borderColor: [
+                    'rgba(0,0,0,1)',
+                    'rgba(0,0,0,1)',
+                    'rgba(0,0,0,1)',
+                    'rgba(0,0,0,1)',
+                    'rgba(0,0,0,1)',                    'rgba(0,0,0,1)',
+                    'rgba(0,0,0,1)',
+                    'rgba(0,0,0,1)',
+                    'rgba(0,0,0,1)',
+                    'rgba(0,0,0,1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            legend:
+                {
+                    display: false
+                },
+            title:
+                {
+                    display: true,
+                    text: "Pick- and Winrates per Legion in %"
+                }
+        }
+    });
+    //calc winrates
+    data = data.stats.legionPickWinRate;
+    var totalgames = 0;
+    var pickchance = [];
+    var winchance = [];
+    for (var i = 0; i < 5; i++) {
+        totalgames += data[i].gamesPicked;
+    }
+    for (var i = 0; i < 5; i++) {
+        pickchance[i] = ((data[i].gamesPicked / totalgames) * 100).toFixed(2);
+        winchance[i] = ((data[i].gamesWon / data[i].gamesPicked) * 100).toFixed(2);
+        addData(myChart, "Pick " + data[i].legion, pickchance[i]);
+        addData(myChart, "Win " + data[i].legion, winchance[i]);
+    }
+}
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+
+}
+
+function showEloDistribution(data) {
+    hideLoad();
+    hideInputs();
+    showStatsPage();
+    createLineGraph(data);
+}
+
+function saveSelection() {
+    type = document.getElementById("typeselector").value;
+    switch (type) {
+        case "version":
+            document.getElementById("value_textField").style.display = "";
+            document.getElementById("value_dateField").style.display = "none";
+            break;
+        case "playerid":
+            document.getElementById("value_textField").style.display = "";
+            document.getElementById("value_dateField").style.display = "none";
+            break;
+        case "tsgte":
+            document.getElementById("value_dateField").style.display = "";
+            document.getElementById("value_textField").style.display = "none";
+            break;
+        case "tslt":
+            document.getElementById("value_dateField").style.display = "";
+            document.getElementById("value_textField").style.display = "none";
+            break;
+        default:
+            break;
+    }
+}
+
+function showWinPickrates() {
+    showInputs();
+    showStatsPage();
+}
+
+function loadWinrates() {
+    if (document.getElementById("value_textField").style.display == "") var meineValue = document.getElementById("value_textField").value;
+    else var meineValue = document.getElementById("value_dateField").value;
+    queryWinRates(document.getElementById("typeselector").value, meineValue);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//api requests
 function getEloDistribution(callback) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
@@ -9,7 +222,9 @@ function getEloDistribution(callback) {
     xhttp.send();
 }
 function queryEloDistribution() {
+    showLoad();
     getEloDistribution(function (result) {
+        showEloDistribution(result);
         return result;
     });
 }
@@ -24,7 +239,9 @@ function getWinRates(callback, type, value) {
     xhttp.send();
 }
 function queryWinRates(type, value) {
+    showLoad();
     getWinRates(function (result) {
+        createBarGraph(result);
         return result;
     }, type, value);
 }
